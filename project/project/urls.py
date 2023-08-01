6aecd51917b694d0bd6ba8805327119a1b251287
David Ryder <67267592+david-ryder@users.noreply.github.com>
2023-08-01 00:45:56 -0400

    url rerouting

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')), # Include the app's URLs under 'api/' prefix
+    path('auth/', include('authentication.urls')), # Include the app's URLs under 'api/' prefix
 ]
